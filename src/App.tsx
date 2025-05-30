8966f45544e9f6401a95fd22125baf270caee935
scavaille <scavaille@users.noreply.github.com>
2025-05-30 18:40:01 +0200

    Add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/geo-photo-filter-fun">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
